7fdec3dc99653c2e2e16593098a46e78d1072840
asakura89 <flamboyan3no1a@gmail.com>
2018-12-20 20:01:31 +0700

    update license and gitignore

--- ColorGen/MainForm.cs
+++ ColorGen/MainForm.cs
@@ -2,26 +2,20 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace ColorGen
-{
-    public partial class MainForm : Form
-    {
-        public MainForm()
-        {
+namespace ColorGen {
+    public partial class MainForm : Form {
+        public MainForm() {
             InitializeComponent();
         }
 
-        private Int32 GetRandomNumber(Int32 lowerBound, Int32 upperBound)
-        {
+        Int32 GetRandomNumber(Int32 lowerBound, Int32 upperBound) {
             Int32 seed = Guid.NewGuid().GetHashCode() % 50001;
             var rnd = new Random(seed);
             return rnd.Next(lowerBound, upperBound);
         }
 
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            try
-            {
+        protected override void OnPaint(PaintEventArgs e) {
+            try {
                 Color redColorSeed = ColorTranslator.FromHtml("#e40b7c");
                 Color greenColorSeed = ColorTranslator.FromHtml("#8ac900");
 
@@ -29,22 +23,20 @@ namespace ColorGen
                 Int32 columnCount = Convert.ToInt32(Math.Ceiling(ClientSize.Width / PatternSize));
                 Int32 rowCount = Convert.ToInt32(Math.Ceiling(ClientSize.Height / PatternSize));
 
-                for (Int32 colIdx = 0; colIdx < columnCount; colIdx++)
-                {
-                    for (Int32 rowIdx = 0; rowIdx < rowCount; rowIdx++)
-                    {
+                for (Int32 colIdx = 0; colIdx < columnCount; colIdx++) {
+                    for (Int32 rowIdx = 0; rowIdx < rowCount; rowIdx++) {
                         Int32 red = (GetRandomNumber(0, 256) + redColorSeed.R) / 2;
                         Int32 green = (GetRandomNumber(0, 256) + redColorSeed.G) / 2;
                         Int32 blue = (GetRandomNumber(0, 256) + redColorSeed.B) / 2;
                         Color randColor = Color.FromArgb(255, red, green, blue);
 
                         //e.Graphics.DrawRectangle(new Pen(randColor), colIdx * PatternSize, rowIdx * PatternSize, PatternSize, PatternSize);
-                        e.Graphics.FillRectangle(new SolidBrush(randColor), colIdx * PatternSize, rowIdx * PatternSize, PatternSize, PatternSize);
+                        e.Graphics.FillRectangle(new SolidBrush(randColor), colIdx * PatternSize, rowIdx * PatternSize,
+                            PatternSize, PatternSize);
                     }
                 }
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex) {
                 MessageBox.Show(ex.Message);
             }
         }
--- ColorGen/Properties/AssemblyInfo.cs
+++ ColorGen/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ColorGen")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ColorGen")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
